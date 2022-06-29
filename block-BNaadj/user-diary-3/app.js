45008ced12174f5fdce26d7a379e0d7a8f52d345
raj shringi <raj24shringi@gmail.com>
2022-06-29 16:33:05 +0530

    create boilerplate for project

---
+++ block-BNaadj/user-diary-3/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/user-diary-3", (err) => {
+  console.log(err ? err : "connected to database");
+});
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(4000, () => {
+  console.log("server is listening on port 4k");
+});
